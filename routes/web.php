707501b5d0d14867b675ffbfcd78cbe80009897b
Konstantin Davydov <davydovks@mail.ru>
2023-09-26 18:02:31 +0500

    add route for articles

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PageController;
+use App\Models\Article;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +20,5 @@ Route::get('/', function () {
 });
 
 Route::get('about', [PageController::class, 'about']);
+
+Route::get('articles', [ArticleController::class, 'index'])->name('articles.index');
