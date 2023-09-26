d3a258db9eaef8445ecbddffd95ba62280ec84f6
Konstantin Davydov <davydovks@mail.ru>
2023-09-26 19:57:50 +0500

    add named routes

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
-Route::get('about', [PageController::class, 'about']);
+Route::get('about', [PageController::class, 'about'])->name('pages.about');
 
 Route::get('articles', [ArticleController::class, 'index'])->name('articles.index');
